f94bdba218fe11b2aae4f082215e01fa46a063c9
Gladiator07 <atharvaaingle@gmail.com>
2021-02-11 11:51:05 +0000

    added paths

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# This script has all the configurations for the dataset
+
+import os
+
+DATA_DIR = "/root/input/chest_xray"
+
+TRAIN_P_DIR = os.path.join(DATA_DIR, "train/PNEUMONIA")
+TRAIN_N_DIR = os.path.join(DATA_DIR, "train/NORMAL")
+
+TEST_P_DIR = os.path.join(DATA_DIR, "test/PNEUMONIA")
+TEST_N_DIR = os.path.join(DATA_DIR, "test/NORMAL")
+
+VALID_P_DIR = os.path.join(DATA_DIR, "test/PNEUMONIA")
+VALID_N_DIR = os.path.join(DATA_DIR, "test/NORMAL")
