bd93adae3d096270feef92ed0aabd41e55c53098
Gladiator07 <atharvaaingle@gmail.com>
2021-02-14 15:14:50 +0000

    added training script

--- src/train.py
+++ src/train.py
@@ -1,9 +1,65 @@
 import torch
 from torch.optim import optimizer
 from model import PneumoniaResnet
+import torch.nn as nn
+from data import val_dl
+import model
+from utils import to_device
+
 
 @torch.no_grad()
 def evaluate(model, val_loader):
     model.eval()
-    outputs = [model.PneumoniaResnet.validation_step(batch) for batch in val_loader]
+    outputs = [model.validation_step(batch) for batch in val_loader]
     return model.validation_epoch_end(outputs)
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader,
+                  weight_decay=0, grad_clip=None, opt_func=torch.optim.SGD):
+    torch.cuda.empty_cache()
+    history = []
+
+    # Set up cutom optimizer with weight decay
+    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
+    # Set up one-cycle learning rate scheduler
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs,
+                                                steps_per_epoch=len(train_loader))
+
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+
+            # Gradient clipping
+            if grad_clip:
+                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Record & update learning rate
+            lrs.append(get_lr(optimizer))
+            sched.step()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+device = 'cuda'
+model = to_device(PneumoniaResnet(), device)
+history = [evaluate(model, val_dl)]
+print(history)
